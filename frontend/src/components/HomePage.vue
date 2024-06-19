b76db368fa27d67c3c1eee166bb8752f8ed81191
ravi <rvk303804@gmail.com>
2024-06-19 12:52:25 +0530

    re-initialized the frontend using vite

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -2,6 +2,7 @@
     <div>
         <h1>This is the home page.</h1>
         <h3 v-if="message">{{ message }}</h3>
+        <h3 v-else>No message available</h3>
     </div>
 </template>
 
@@ -16,20 +17,28 @@ export default {
         };
     },
     mounted() {
-        const url = `${process.env.VUE_APP_BACKEND_URL}/home`;
+        const backendUrl = import.meta.env.VITE_BACKEND_URL;
+        console.log(backendUrl)
+        const url = `${backendUrl}/home`;
+
+        console.log('Backend URL:', backendUrl);
+        console.log('Full URL:', url);
+
         axios.get(url)
             .then(response => {
+                console.log('Response data:', response.data);
                 if (response.data && response.data.message) {
                     this.message = response.data.message;
                 }
             })
             .catch(error => {
                 console.error('There was an error fetching the data!', error);
+                this.message = 'An error occurred while fetching the data.';
             });
     }
 }
 </script>
 
 <style scoped>
-
+/* Add your styles here */
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,10 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./routers";
 
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './routers';
+import "./styles/index.css";
 
 const app = createApp(App);
 
 app.use(router);
-app.mount('#app');
+app.mount("#app");

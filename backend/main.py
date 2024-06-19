3f2ebcd5be180f85ab3886d3f8756754c439951d
namansehwal <namansehwal@gmail.com>
2024-06-19 15:37:07 +0530

    Added sample data route

--- backend/main.py
+++ backend/main.py
@@ -2,19 +2,31 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  
+CORS(app)
 
-@app.route('/')
+
+@app.route("/")
 def homepage():
     return "<h1>Backend Server is running</h1>"
 
-@app.route('/home')
+
+@app.route("/home")
 def home():
     response = {
         "success": True,
-        "message": "Successfully received data from the backend"
+        "message": "Successfully received data from the backend",
     }
     return jsonify(response)
 
-if __name__ == '__main__':
+
+@app.route("/data")
+def data():
+    response = {
+        "success": True,
+        "data": [{"name": "John Doe", "age": 30}, {"name": "Jane Doe", "age": 25}],
+    }
+    return jsonify(response)
+
+
+if __name__ == "__main__":
     app.run(debug=True)

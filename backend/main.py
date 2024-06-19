e291d67b6b227df067fc4241683dd967e2b6f65e
namansehwal <namansehwal@gmail.com>
2024-06-19 17:28:44 +0530

    chore: Add student summary API route and related files

--- backend/main.py
+++ backend/main.py
@@ -1,31 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-
-@app.route("/")
-def homepage():
-    return "<h1>Backend Server is running</h1>"
-
-
-@app.route("/home")
-def home():
-    response = {
-        "success": True,
-        "message": "Successfully received data from the backend",
-    }
-    return jsonify(response)
-
-
-@app.route("/data")
-def data():
-    response = {
-        "success": True,
-        "data": [{"name": "John Doe", "age": 30}, {"name": "Jane Doe", "age": 25}],
-    }
-    return jsonify(response)
+# Importing API routes
+from routes import *
 
 
 if __name__ == "__main__":

8ca19b3243a5502804b82323c59a4530fd6ebbdd
Lokesh kuamar <lokeshkumarmahawar78@gmail.com>
2024-06-19 17:27:00 +0530

    1st commit

---
+++ frontend/src/components/Layout.vue
@@ -0,0 +1,17 @@
+<!-- src/components/Layout.vue -->
+<template>
+    <div>
+      <Navbar />
+      <router-view />
+    </div>
+  </template>
+  
+  <script>
+  import Navbar from './Navbar.vue';
+  
+  export default {
+    components: {
+      Navbar
+    }
+  }
+  </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./routers";
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { FcGoogle } from "oh-vue-icons/icons";
 
 import "./styles/index.css";
 
+addIcons(FcGoogle);
+
 const app = createApp(App);
 
+app.component("v-icon", OhVueIcon);
 app.use(router);
 app.mount("#app");

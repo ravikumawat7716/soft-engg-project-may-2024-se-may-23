2318b2a7d0d0047ab5a4528145631973560ce9f7
namansehwal <namansehwal@gmail.com>
2024-07-10 22:56:44 +0530

    feat: Add test route for the gmeet

--- backend/routes.py
+++ backend/routes.py
@@ -7,5 +7,11 @@ def homepage():
     return "<h1>Backend Server is running</h1>"
 
 
+# test route
+@app.route("/test")
+def test():
+    return "<h1>Test route for the gmeet✌️</h1>"
+
+
 # Student Routes
 app.add_url_rule("/student_summary", view_func=student_summary, methods=["GET"])

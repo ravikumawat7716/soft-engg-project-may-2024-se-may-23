e28f942168bcb11d9929495314a6922078fdfa83
namansehwal <namansehwal@gmail.com>
2024-07-13 19:44:27 +0530

    modified:   backend/utils/LLM.py 	modified:   backend/utils/configuration.py

--- backend/utils/LLM.py
+++ backend/utils/LLM.py
@@ -8,3 +8,25 @@ def generate(prompt, model="qwen2:0.5b"):
         prompt=prompt,
     )
     return result["response"]
+
+
+def ChatBot(chat, model="qwen2:0.5b"):
+    stream = ollama.chat(
+        model=model,
+        messages=chat,
+        stream=True,
+    )
+
+    for chunk in stream:
+        print(chunk["message"]["content"], end="", flush=True)
+
+
+ChatBot([{"role": "user", "content": "Why is the sky blue?"}])
+
+chats = [
+    {"role": "user", "content": "Why is the sky blue?"},
+    {"role": "assistant", "content": "The sky is blue because of Rayleigh scattering."},
+]
+
+
+l
--- backend/utils/configuration.py
+++ backend/utils/configuration.py
@@ -5,12 +5,12 @@ from apis.login import UserLoginAPI
 from apis.summary import StudentSummaryAPI
 from apis.gen_ai import Gen_AI
 from apis.video_summary import Video_Summary
-from apis.notes_generator import NotesGenerator
 
 
 def create_app():
     print("Creating app...")
     CORS(app)
+
     api.add_resource(UserLoginAPI, "/login")
     api.add_resource(StudentSummaryAPI, "/student_summary")
 
@@ -18,10 +18,13 @@ def create_app():
     api.add_resource(Gen_AI, "/gen_ai")
 
     # Adding the notes generator API
-    api.add_resource(NotesGenerator, '/notes_generator')
+    api.add_resource(NotesGenerator, "/notes_generator")
 
     # Adding the Video Summary API
     api.add_resource(Video_Summary, "/video_summary")
+
+    # AI chatbot
+    # api.add_resource(ChatBot, "/chatbot")
 
     api.init_app(app)
     return app

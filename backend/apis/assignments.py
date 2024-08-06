00f40604358ded0028d816b55b18f629dda1a2f2
Aradhana-Anand <21f1002982@ds.study.iitm.ac.in>
2024-08-06 19:55:12 +0530

    implement create_assignment

--- backend/apis/assignments.py
+++ backend/apis/assignments.py
@@ -3,16 +3,46 @@
 from flask_restful import Resource
 from flask import jsonify, request
 from instances import api
+from model.connection import mongo_handler
+from datetime import datetime
+from bson.json_util import dumps, ObjectId
+
 
 
 class CreateAssignmentAPI(Resource):
     def post(self):
+        data = request.get_json()
+        courseId = data.get('courseId')
+        assignmentName = data.get('assignmentName')
+        description = data.get('description')
+
+        if not assignmentName or not description or not courseId :
+            return {"message": "assignmentName, description and courseId are required"}, 400
+
+        new_assignment = {
+            "assignmentName": assignmentName,
+            "description": description,
+            "createdAt": datetime.utcnow(),
+            "updatedAt": datetime.utcnow()
+        }
+               
         try:
-            return jsonify({"message":"Create a new assignment"})
+            course = mongo_handler.get_document_by_field("CourseCluster", "_id", ObjectId(courseId))
+            if course:
+                updated_assignments = course["assignments"]
+                updated_assignments.add(new_assignment)
+                update_data = {
+                    "assignments": updated_assignments,
+                    "updatedAt": datetime.utcnow()
+                }
+                mongo_handler.update_document("CourseCluster", "_id", ObjectId(courseId), update_data)
+                return jsonify({"message": "Assignment created successfully."})
+            else:
+                return {"message": "Invalid course ID format"}, 400
 
         except Exception as e:
-            return jsonify({"error": str(e)}), 500  # Proper error handling
-        
+            return {"message": "Invalid course ID format"}, 400
+      
         
 class GetCourseAssignmentsAPI(Resource):
     def get(self, courseId):

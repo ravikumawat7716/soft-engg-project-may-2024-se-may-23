8934124942b7337d7d1e1f55132c94e22f301fe8
Lokesh kuamar <lokeshkumarmahawar78@gmail.com>
2024-08-12 13:22:51 +0530

    initially_code_explainer_added_in_frontend

--- frontend/src/components/CodeEditor.jsx
+++ frontend/src/components/CodeEditor.jsx
@@ -2,11 +2,15 @@ import { useRef, useState } from "react";
 import { Editor } from "@monaco-editor/react";
 import { CODE_SNIPPETS } from "../constants";
 import Output from "./Output";
+import axios from "axios";
+import { ApiUrl } from "../config";
 
 const CodeEditor = () => {
   const editorRef = useRef();
   const [value, setValue] = useState("");
   const [language, setLanguage] = useState("python");
+
+  const [codeDetail, setCodeDetail] = useState(null);
 
   const onMount = (editor) => {
     editorRef.current = editor;
@@ -16,6 +20,20 @@ const CodeEditor = () => {
   const onSelect = (language) => {
     setLanguage(language);
     setValue(CODE_SNIPPETS[language]);
+  };
+
+  const codeExplainer = async () => {
+    const res = await axios({
+      url: `${ApiUrl}/code_explanation`,
+      data: editorRef.current.getValue(),
+      method: "POST",
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+
+    setCodeDetail(res.data.result);
+    console.log(res.data.result);
   };
 
   return (
@@ -41,6 +59,18 @@ const CodeEditor = () => {
           />
         </div>
         <Output className="w-[40%]" editorRef={editorRef} language={language} />
+        <div className="flex flex-col">
+          {editorRef && (
+            <button
+              className="px-4 py-2 text-white rounded-full bg-red-700"
+              onClick={codeExplainer}
+            >
+              Explain Code
+            </button>
+          )}
+
+          {codeDetail && <h1>{codeDetail}</h1>}
+        </div>
       </div>
     </div>
   );
--- frontend/src/pages/ProgrammingAssignment.jsx
+++ frontend/src/pages/ProgrammingAssignment.jsx
@@ -60,13 +60,6 @@ const ProgrammingAssignment = () => {
         <div>
           <div className="flex mt-4 w-full justify-between">
             <h1 className="font-semibold text-lg">{pa.title}</h1>
-
-            <button
-              onClick={toggleModal}
-              className="mb-0 px-4 py-2 text-sm bg-red-700 text-white rounded-full transition-colors duration-300"
-            >
-              AI Support
-            </button>
           </div>
 
           <div className="flex flex-col md:flex-row gap-3 w-full transition-all duration-500">
@@ -80,56 +73,6 @@ const ProgrammingAssignment = () => {
                 assignment_id={params.paId}
                 question={pa.title}
               />
-            </div>
-
-            <div
-              className={`chat-bot-div ${
-                isModalOpen ? "w-[90%] h-[500px] md:w-[30%]" : "hidden"
-              } border-2 border-gray-400 rounded-md h-[500px] mt-14 flex flex-col transition-all duration-500`}
-            >
-              <div className="flex-1 p-4 overflow-y-auto">
-                {messages.length > 0 ? (
-                  messages.map((message, index) => (
-                    <div key={index} className="mb-2">
-                      <div
-                        className={`p-2 rounded ${
-                          message.from === "bot"
-                            ? "bg-gray-200"
-                            : "bg-blue-500 text-white"
-                        }`}
-                      >
-                        {message.text}
-                      </div>
-                    </div>
-                  ))
-                ) : (
-                  <div className="h-full w-full flex items-center justify-center">
-                    <div className="flex flex-col gap-2">
-                      <span className="text-center text-4xl">
-                        <span className="icon-large">🤖</span>{" "}
-                      </span>
-                      <span>Hi User, I am AI. How can I help you?</span>
-                    </div>
-                  </div>
-                )}
-              </div>
-              <div className="p-4 border-t border-gray-300 flex">
-                <textarea
-                  value={newMessage}
-                  onChange={handleInputChange}
-                  onKeyUp={(e) => e.key === "Enter" && sendMessage()}
-                  placeholder="Type your query..."
-                  className="flex-1 p-2 outline-none border px-4 py-2 text-sm rounded-md resize-none overflow-hidden"
-                  rows="1"
-                  ref={textareaRef}
-                ></textarea>
-                <button
-                  onClick={sendMessage}
-                  className="ml-2 px-4 py-2 text-white bg-red-700 rounded-md transition-colors duration-300"
-                >
-                  Go
-                </button>
-              </div>
             </div>
           </div>
         </div>
